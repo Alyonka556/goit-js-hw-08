8bddfc41900eaa17c98c64edbd81f352d7d8fa3f
Alyonka556 <142902721+Alyonka556@users.noreply.github.com>
2023-11-08 20:09:09 +0100

    summary hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+
+const VCT_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
+
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(VCT_KEY, JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(VCT_KEY)) || 0);
+
 
